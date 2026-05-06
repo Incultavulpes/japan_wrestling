6f183d1ba454a3c4f87b18640d39a6f3e8e1cf2d
VulpesInculta <fernandorevengaperez@gmail.com>
2026-05-06 13:06:51 +0200

    DE: Implement data discovery utility in clean_master
    1. Added 'tell_data' function to dynamically load and preview CSVs.
    2. Implemented os.path logic to safely navigate between uww_raw and wiki_processed folders.
    3. Created an interactive terminal interface for file selection.
    4. Established the foundation for the upcoming standardization and merging logic.

---
+++ clean_master.py
@@ -0,0 +1,25 @@
+import pandas as pand
+import os.path
+
+df_aux = pand.read_csv("data/uww_raw/uww_rank.csv")
+
+def tell_data(file_handle, file_type):
+    if file_type.lower() == "uww":
+        usual_path = os.path.join("data", "uww_raw")
+    elif file_type.lower() == "wikipedia":
+        usual_path = os.path.join("data", "processed")
+    else:
+        print("Non existing file type")
+        return None
+
+    if os.path.isfile(os.path.join(usual_path, file_handle)):
+        df = pand.read_csv(os.path.join(usual_path, file_handle))
+        print(df)
+    else:
+        print("Non existing file handle, run the script again")
+
+
+file_handle = input("Enter the file handle ")
+file_type = input("Enter the file type ")
+
+tell_data(file_handle, file_type)
